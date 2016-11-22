fbc35c7dc3b0a44320e49bfece03b7312c924ea8
pope <pettazz@gmail.com>
2016-11-21 23:49:18 -0500

    forgot the entire goddamned point of the thing

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ authd_client = fitbit.Fitbit(
 
 while True:
     elapsed = datetime.datetime.now() - start
+    print "elapsed: %s" % elapsed
     if elapsed > datetime.timedelta(days=1):
+        print "doing the damn thing"
         authd_client.log_sleep(datetime.datetime.now() - datetime.timedelta(days=0), 27000000)
         start = datetime.datetime.now()
     else:
+        print "refreshing token"
+        print authd_client.client.refresh_token()
+        print "sleeping"
         time.sleep(3600)
